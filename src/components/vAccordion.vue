097394e95114fb9a66f6554fb0ff37b572bb165c
KBuchynskyi <kyrylo.buchynskyi@onseo.biz>
2019-10-17 14:47:09 +0300

    added orientation prop

--- src/components/vAccordion.vue
+++ src/components/vAccordion.vue
@@ -1,70 +1,75 @@
 <template>
-  <div class="v-accordion" :class="{horizontal: horizontal}">
+  <div class="v-accordion" :class="{horizontal: horizontal, vertical: !horizontal}">
     <slot></slot>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    initialTabIndex: {
-      type: Number,
-      default: -1
-    },
-    horizontal: {
-      type: Boolean,
-      default: false
-    } 
-  },
-  data() {
-    return {
-      activeTabIndex: -1
+    export default {
+        props: {
+            initialTabIndex: {
+                type: Number,
+                default: -1
+            },
+            horizontal: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                activeTabIndex: -1
+            };
+        },
+        computed: {
+            tabs() {
+                return this.$children;
+            }
+        },
+        methods: {
+            toggleTab(index) {
+                this.activeTabIndex = index !== this.activeTabIndex ? index : -1;
+                this.tabs.forEach((tab, i) =>
+                    tab.setVisibility(i === this.activeTabIndex)
+                );
+            },
+            setTabIndex() {
+                this.tabs.forEach((tab, i) => (tab.index = i));
+            },
+            setInitialIndex() {
+                this.initialTabIndex !== -1 && this.openTabByIndex(this.initialTabIndex);
+            },
+            openTabByIndex(index) {
+                this.tabs[index].setVisibility(true);
+            },
+            init() {
+                this.setTabIndex();
+                this.setInitialIndex();
+                this.$on("tabToggled", this.toggleTab);
+            }
+        },
+        watch: {
+            tabsOrientation() {
+                this.tabs.forEach(tab => (tab.horizontal = this.horizontal));
+            }
+        },
+        mounted() {
+            this.init();
+        }
     };
-  },
-  computed: {
-    tabs() {
-      return this.$children;
-    }
-  },
-  methods: {
-    toggleTab(index) {
-      this.activeTabIndex = index !== this.activeTabIndex ? index : -1;
-      this.tabs.forEach((tab, i) =>
-        tab.setVisibility(i === this.activeTabIndex)
-      );
-    },
-    setTabIndex() {
-      this.tabs.forEach((tab, i) => (tab.index = i));
-    },
-    setTabOrientation() {
-      this.tabs.forEach(tab => (tab.horizontal = this.horizontal));
-    },
-    setInitialIndex() {
-      this.initialTabIndex !== -1 && this.openTabByIndex(this.initialTabIndex);
-    },
-    openTabByIndex(index) {
-      this.tabs[index].setVisibility(true);
-    },
-    init() {
-      this.setTabIndex();
-      this.setTabOrientation();
-      this.setInitialIndex();
-      this.$on("tabToggled", this.toggleTab);
-    }
-  },
-  mounted() {
-    this.init();
-  }
-};
 </script>
 
 <style scoped>
-.v-accordion {
-  display: flex;
-  flex-direction: column;
-}
+  .v-accordion {
+    display: flex;
+    box-sizing: border-box;
+    flex-flow: column nowrap;
+    width: 400px;
+  }
 
-.v-accordion.horizontal  {
-  flex-direction: row;
-}
+  .v-accordion.horizontal {
+    flex-flow: row nowrap;
+    height: 300px;
+    width: 700px;
+  }
 </style>
--- src/components/vAccordionTab.vue
+++ src/components/vAccordionTab.vue
@@ -1,59 +1,139 @@
 <template>
-  <div @click="toggleTab" class="accordion-tab" :class="{horizontal: horizontal}">
-    <div class="tab-head">Tab {{index + 1}}</div>
-    <div class="tab-body" v-show="opened">
-      <slot></slot>
+  <div @click="toggleTab" class="v-accordion-tab"
+       :class="{horizontal: horizontal, vertical: !horizontal, active: opened}">
+    <div class="tab-head">
+      <slot name="tab-head">
+        <p class="tab-title">v-accordion-tab</p>
+      </slot>
+    </div>
+    <div class="tab-body">
+      <slot name="tab-body">
+      </slot>
     </div>
   </div>
 </template>
 
 
 <script>
-export default {
-  data() {
-    return {
-      opened: false,
-      horizontal: false,
-      index: 0
+    export default {
+        data() {
+            return {
+                opened: false,
+                horizontal: false,
+                index: 0
+            };
+        },
+        methods: {
+            setVisibility(visible) {
+                this.opened = visible;
+            },
+            toggleTab() {
+                this.$parent.$emit("tabToggled", this.index);
+            }
+        }
     };
-  },
-  methods: {
-    setVisibility(visible) {
-      this.opened = visible;
-    },
-    toggleTab() {
-      this.$parent.$emit("tabToggled", this.index);
-    }
-  }
-};
 </script>
 
 <style scoped>
-.accordion-tab {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2px;
-}
 
-.accordion-tab.horizontal {
-  flex-direction: row;
-}
+  .v-accordion-tab {
+    display: flex;
+    flex-flow: column nowrap;
+  }
 
-.tab-head {
-  padding: 2%;
-  font: 20px Arial;
-  color: #555;
 
-  background: #41b883;
-}
+  .horizontal .v-accordion-tab {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: stretch;;
+    height: auto;
+  }
 
-.accordion-tab.active {
-  opacity: 1;
-}
+  .v-accordion-tab {
+    transition: height ease-in 0.3s;
+    height: 52px;
+  }
 
-.tab-body {
-  min-height: 30px;
+  .v-accordion-tab.active {
+    height: 250px;
+  }
 
-  background: #eee;
-}
+  .horizontal .v-accordion-tab {
+    transition: width ease-in 0.3s;
+    width: 58px;
+    height: auto;
+  }
+
+  .horizontal .v-accordion-tab.active {
+    width: 500px;
+  }
+
+  .horizontal .v-accordion-tab .tab-body > div {
+    height: 100%;
+    transition-duration: 0.1s;
+    opacity: 0;
+  }
+
+  .horizontal .v-accordion-tab.active .tab-body > div {
+    opacity: 1;
+    transition: opacity 0.4s linear 0.4s;
+  }
+
+  .tab-head {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    background: #29b6f6;
+    box-shadow: 2px 0px 2px #e9eaeb;
+    cursor: pointer;
+  }
+
+  .tab-head > div {
+    display: flex;
+    flex-flow: row nowrap;
+  }
+
+  .horizontal .tab-head {
+    width: 52px;
+    min-width: 52px;
+    height: auto;
+  }
+
+  .tab-head {
+    justify-content: center;
+    width: 100%;
+    height: 46px;
+    min-height: 46px;
+    background: #35495e;
+  }
+
+  .active .tab-head {
+    background: #41b883;
+  }
+
+  .tab-head .tab-title {
+    display: flex;
+    white-space: nowrap;
+    color: #fff;
+    font: 18px Arial;
+  }
+
+  .horizontal .tab-title {
+    transform: rotate(270deg);
+  }
+
+  .tab-body {
+    height: 300px;
+    margin: 6px 0;
+
+    overflow: hidden;
+    background: #f3acac;
+  }
+
+  .horizontal .tab-body {
+    width: 500px;
+    margin: 0 6px;
+  }
+
 </style>
